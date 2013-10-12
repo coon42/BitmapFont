b5f0814bf03c5896a8941f5b70cfb69527ddb2d3
coon <coon@c-base.org>
2013-10-12 06:16:54 +0200

    fixed bug where chars got cut wrongly

--- BitmapFont.py
+++ BitmapFont.py
@@ -1,31 +1,33 @@
 import pygame
 
 
-class BitmapChar():
-    def __init__(self, font_surface, width, height):
-        self.font_surface = font_surface
-        self.width = width
-        self.height = height
+class _BitmapChar():
+    def __init__(self, font_surface, character, left, top, right, bottom):
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.character = character
 
     def get_width(self):
-        return self.width
+        return self.right - self.left
 
     def get_height(self):
-        return self.height
+        return self.bottom - self.top
 
 
 class BitmapFont:
     def __init__(self):
         # index starts at 0x20h
         self.ascii_table = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-        self.letter_pos_dict = {}
+        self.char_dict = {}
+        self.font_surface = None
+        self.surf_arr = None
+        self.SEARCH_NEXT_CHAR, self.GET_CHAR_WIDTH = range(2)
 
     def load_bitmap_font(self, path):
         self.font_surface = pygame.image.load(path).convert() # convert bitmap format to screen formal (pixel depth etc...)
         self.surf_arr = pygame.surfarray.array2d(self.font_surface.convert(8))
-        print "Debug: surf_arr: "
-        print self.surf_arr
-
         self._scan_letter_coords()
         
     def draw_bitmap_text(self, surface, x_pos, y_pos, text):
@@ -34,81 +36,62 @@ class BitmapFont:
 
         for char in text:
             self.put_bitmap_char(surface, cur_x_pos, cur_y_pos, char)
-            char_width = self.letter_pos_dict[char][1][0] - self.letter_pos_dict[char][0][0] - 1
+            char_width = self.char_dict[char].get_width()
             cur_x_pos += char_width
  
     def put_bitmap_char(self, surface, x_pos, y_pos, char):
-        letter_info = self.letter_pos_dict[char]
+        char = self.char_dict[char]
+        surface.blit( self.font_surface, (x_pos, y_pos), pygame.Rect(char.left, char.top, char.get_width(), char.get_height()) )
 
-        if letter_info != None:
-            font_x_pos1 = letter_info[0][0]
-            font_y_pos1 = letter_info[0][1]
-            font_x_pos2 = letter_info[1][0]
-            font_y_pos2 = letter_info[1][1]
 
-            surface.blit(self.font_surface, 
-                        (x_pos, y_pos), pygame.Rect( (font_x_pos1 + 1, font_y_pos1 + 1), (font_x_pos2 - font_x_pos1 - 1, font_y_pos2)))
-
-    def _scan_letter_coords(self):  
-        self.letter_pos_dict = {}
-
-        self.SEARCH_NEXT_LETTER, self.GET_LETTER_LENGTH = range(2)
-        self.state = self.SEARCH_NEXT_LETTER
-        all_letters_found = False
+    def _scan_letter_coords(self):
+        self.state = self.SEARCH_NEXT_CHAR
         self._cur_x_pos = 0
-        self._last_letter_x_pos = 0
-        self._cur_letter_index  = -1
+        self._last_char_x_pos = 0
+        self._cur_char_index  = -1
 
-        while(not all_letters_found):
-            letter_info = self._get_next_letter()
+        all_chars_found = False
+        while(not all_chars_found):
+            char = self._get_next_char()
 
-            if letter_info != None:
-                letter = letter_info[0]
-                x_pos1 = letter_info[1][0][0]
-                y_pos1 = letter_info[1][0][1]
-                x_pos2 = letter_info[1][1][0]
-                y_pos2 = letter_info[1][1][1]
-
-                self.letter_pos_dict[letter] = ( (x_pos1, y_pos1), (x_pos2, y_pos2) )
-                print "Debug: found letter: '%s' between: (x:%d, y:%d) - (x:%d, y:%d)" % (letter, x_pos1, y_pos1, x_pos2, y_pos2)
+            if char != None:
+                self.char_dict[char.character] = char
+                print "Debug: found letter: '%s' between: (x:%d, y:%d) - (x:%d, y:%d)" % (char.character, char.left, char.top, char.right, char.bottom)
             else:
+                all_chars_found = True
                 print "end of bitmap."
 
-            all_letters_found = True if letter_info == None else False
-
         # at last fill all undefined letters with rhe default "?" char position.
-        #for  self.letter_pos_dict
+        # for self.letter_pos_dict
 
 
-        return self.letter_pos_dict
-
-
-    def _get_next_letter(self):
+    def _get_next_char(self):
         x_pos_start = 0
         x_pos_end = 0
 
         end_of_bitmap = False
         while(not end_of_bitmap):
-            if self.state == self.SEARCH_NEXT_LETTER:
-                x_pos_start = self._last_letter_x_pos
-                self.state = self.GET_LETTER_LENGTH
+            if self.state == self.SEARCH_NEXT_CHAR:
+                x_pos_start = self._last_char_x_pos
+                self.state = self.GET_CHAR_WIDTH
 
-            elif self.state == self.GET_LETTER_LENGTH:
+            elif self.state == self.GET_CHAR_WIDTH:
                 next_key_pos = self._get_next_key_pixel_x_pos()
-                self._cur_letter_index += 1
+                self._cur_char_index += 1
 
                 if next_key_pos == None:
                     end_of_bitmap = True
-                elif next_key_pos - self._last_letter_x_pos > 2:
-                    x_pos_start = self._last_letter_x_pos
+                elif next_key_pos - self._last_char_x_pos > 2:
+                    x_pos_start = self._last_char_x_pos
                     x_pos_end = next_key_pos 
-                    self._last_letter_x_pos = x_pos_end
-                    
-                    return (self.ascii_table[self._cur_letter_index], ((x_pos_start, 0), (x_pos_end, self.font_surface.get_height())))
+                    self._last_char_x_pos = x_pos_end
+
+                    char = _BitmapChar(self.font_surface, self.ascii_table[self._cur_char_index], x_pos_start, 1, x_pos_end - 1, self.font_surface.get_height())
+                    return char
                 else:
                     x_pos_start = next_key_pos
                     self._last_letter_x_pos = x_pos_start 
-                    self.state = self.SEARCH_NEXT_LETTER
+                    self.state = self.SEARCH_NEXT_CHAR
             
     def _get_next_key_pixel_x_pos(self):
         key_value = 127
